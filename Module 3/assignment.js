560dd1cba22d08c8e9a67029ac49115e381d3a68
Alec Engebretson <alec.engebretson@doane.edu>
2020-12-21 08:44:57 -0600

    Module 3 Assignment

--- Module 3/assignment.js
+++ Module 3/assignment.js
@@ -20,10 +20,10 @@ function validateANDadd() {
         // so add the word to the proper table 
         if (theNewNumber==1){
            var tableRef = document.getElementById("myList1");
-           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;}
-        else{
+           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + ': ' + PalindromeCheck1(theNewWord);}
+        else {
            var tableRef = document.getElementById("myList2");
-           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;}
+           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + ': ' + PalindromeCheck2(theNewWord);;}
         // erase the form fields
         document.forms["myForm"]["newWord"].value = "";
         document.forms["myForm"]["newNumber"].value = "";
@@ -41,4 +41,32 @@ function validateANDadd() {
     // clear the table of all rows
     var tableRef = document.getElementById("myList2");
     tableRef.innerHTML = " ";
+  }
+
+  function PalindromeCheck1 (theword){
+    // This algorithm creates a string that is the reverse 
+    //   of the original string and then compares the strings 
+
+    // create a string the reverse of the original
+        // Step 1. Use the split() method to return a new array of characters
+        // Step 2. Use the reverse() method to reverse the new created array
+        // Step 3. Use the join() method to join all elements of the array into a string
+    var reverseword = theword.split("").reverse().join("");  // thank you freecodecamp.com
+    // compare them using the ===
+    return theword === reverseword;
+  }
+
+  function PalindromeCheck2 (theword){
+    // This algorithm uses iteration to loop through the first half of the string
+    //   and compare each character with the appropriate character in the second half of the string
+
+    // assume it is a palindrome
+    var palindrome = true;
+
+    var middle = theword.length / 2;
+    for (i=0; i <=  middle; i++){
+      if (theword[i] != theword[theword.length - i - 1])
+            palindrome = false; 
+    } 
+    return palindrome;
   }
--- Module 4/exercise.js
+++ Module 4/exercise.js
@@ -11,19 +11,19 @@ function validateANDadd() {
     // validate that a 1 or 2 was entered as a number
     else if ((theNewNumber != 1) && (theNewNumber != 2)) {
       // a 1 or 2 was not entered as the number so tell user and clear the field
-      alert("Please enter a 1 or 2 for the list.");
+      alert("Please enter a 1 or 2 for the algorithm to use.");
       document.forms["myForm"]["newNumber"].value = "";
       return false;
     }
     else {
-        // a word was entered and a 1 or 2 was entered as the number 
-        // so add the word to the proper table 
-        if (theNewNumber==1){
-           var tableRef = document.getElementById("myList1");
-           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;}
-        else{
-           var tableRef = document.getElementById("myList2");
-           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;}
+        // a word was entered and a 1 or 2 was entered as the number so add it to the table
+        // build the Javascript object
+        var obj = { word: theNewWord, number: theNewNumber };
+        // convert the Javascript object to a JSON string
+        var myJSON = JSON.stringify(obj);
+        // add the string to the table
+        var tableRef = document.getElementById("myJSONtable");
+        (tableRef.insertRow(tableRef.rows.length)).innerHTML = myJSON + ",";
         // erase the form fields
         document.forms["myForm"]["newWord"].value = "";
         document.forms["myForm"]["newNumber"].value = "";
@@ -31,14 +31,8 @@ function validateANDadd() {
     }
   }
 
-  function clearList1() {
+  function clearFile() {
     // clear the table of all rows
-    var tableRef = document.getElementById("myList1");
-    tableRef.innerHTML = " ";
-  }
-
-  function clearList2() {
-    // clear the table of all rows
-    var tableRef = document.getElementById("myList2");
+    var tableRef = document.getElementById("myJSONtable");
     tableRef.innerHTML = " ";
   }
